8530701b846ca1a6d509c479795f8e6e2e4aabe5
Baptiste <baptiste.bereni@edu.devinci.fr>
2025-07-02 09:50:17 +0200

    frontend modif

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.querySelector("#button").addEventListener("click", () => {
-  const firstName = document.querySelector("#nom").value;
-  const lastName = document.querySelector("#prenom").value;
+  const departure = document.querySelector("#departure").value;
+  const arrival = document.querySelector("#arrival").value;
   //   console.log(firstName);
   //   console.log(lastName);
-  fetch("http://localhost:3000/name", {
+  fetch("http://localhost:3000/FindOneTrips", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ firstName: firstName, lastName: lastName }),
+    body: JSON.stringify({ departure: departure, arrival: arrival }),
   })
     .then((res) => res.json())
     .then((data) => {
-      document.querySelector("#affichageNom").textContent = data.firstName;
-
-      document.querySelector("#affichangePrenom").textContent = data.lastName;
+      //   document.querySelector(".card result-card").innerHTML = `
+      //   <hr />
+      //       <p>${data.departure}</p>`;
+      //   document.querySelector("#affichangePrenom").textContent = data.lastName;
     });
 });
